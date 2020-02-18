2ff46ce5b06f973f6c27922498c3831776c69335
Stan <zhavoronkov@vistgroup.ru>
2020-02-18 17:56:38 +0300

    add example

--- infrastructure/server/main.go
+++ infrastructure/server/main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+func getEnv(key string, fallback string) (string, error) {
+	if value := os.Getenv(key); value != "" {
+		return value, nil
+	}
+	return fallback, nil
+}
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	port, err := getEnv("PORT", "8001")
+	if err != nil {
+		log.Fatalf("[config] %s\n", err.Error())
+	}
+	http.HandleFunc("/fast", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Header)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("HELLO"))
 	})
-	err := http.ListenAndServe(":8002", nil)
+	http.HandleFunc("/nocache", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header)
+		w.Header().Add("Cache-Control", "no-cache")
+		w.Header().Add("Pragma", "no-cache")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("NO CACHE HELLO"))
+	})
+	http.HandleFunc("/slow", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header)
+		time.Sleep(5 * time.Second)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("SLOW HELLO"))
+	})
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./files"))))
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
